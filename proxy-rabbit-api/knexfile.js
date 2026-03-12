85ef3e1ee94a992130bbb9cc9194f09807d2f1bf
Stephen Man <stephenman88@gmail.com>
2026-03-12 02:52:11 -0400

    knexfile no longer requires ssl

--- proxy-rabbit-api/knexfile.js
+++ proxy-rabbit-api/knexfile.js
@@ -11,6 +11,9 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      charset: "utf8"
+      charset: "utf8",
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
 };
